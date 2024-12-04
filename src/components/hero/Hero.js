218dc0e9315edd831bf2621f40c994c3908dddfb
Himanshu Kumar Sharma <himanshukumarsharma@Himanshu-ka-MacBook-Air.local>
2024-12-05 02:49:04 +0530

    x

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,9 +7,7 @@ export default function Hero() {
         Hi, I'm Himanshu,curious thinker and storyteller crafting{" "}
         <span className="purple">Digital experiences.</span>
       </h1>
-      {/* <p className="subheading">
-        I&#39;m Himanshu. Inquisitive UX Designer, storyteller and bookworm.
-      </p> */}
+      
     </div>
   );
 }
