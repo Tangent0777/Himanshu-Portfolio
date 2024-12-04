d8e05450e756520abc7e3db90031a8b50ef7b7c4
himanshu kumar sharma <hs0241583@gmail.com>
2024-12-05 03:00:23 +0530

    x

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="hero mainbox">
       <h1>
-        Hi, I'm Himanshu,curious thinker and storyteller crafting{" "}
+        Hi, I&apos;m Himanshu,curious thinker and storyteller crafting{" "}
         <span className="purple">Digital experiences.</span>
       </h1>
       
