4bb737b46b3954d86ec9fbb72a9fa2e3bc955d8d
Himanshu Kumar Sharma <himanshukumarsharma@Himanshu-ka-MacBook-Air.local>
2024-12-05 02:18:31 +0530

    changes made

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <div className="hero mainbox">
       <h1>
-        product designer and storyteller working to make a{" "}
-        <span className="purple">big impact.</span>
+        Hi, I 'm Himanshu,curious thinker and storyteller crafting{" "}
+        <span className="purple">Digital experiences.</span>
       </h1>
       {/* <p className="subheading">
         I&#39;m Himanshu. Inquisitive UX Designer, storyteller and bookworm.
